fe0e7a4402991157a0c61f6c278b9e36c0df1d91
Agnaldo Marinho <agnaldo@MacBook-Pro-de-Agnaldo.local>
2021-04-04 18:37:50 -0300

    Create copy colletion

--- copy-colletion/src/index.js
+++ copy-colletion/src/index.js
@@ -24,6 +24,46 @@ const loadDbFromUrl = async (mongoUrl) =>  {
     }
 }
 
+const copyColletion = async (source, target, name, bar) => {
+    try {
+        return new Promise(async (res, rej) => {
+            const sourceColletion = await source.colletion(name);
+            const targetColletion = await target.colletion(name);
+            const allData = await sourceCollection.find().toArray();
+            await Promise.all(allData.map(async (d) => {
+                try {
+                    if (currData == 0) {
+                        await bar.progress.start(bar.globalCountOfData, 0);
+                    }
+    
+                    await targetCollection.inset(d, { safe: true });
+    
+                    bar.progress.update(++currData, {
+                        speed: name
+                    });
+                    if (currData == bar.globalCountOfData) {
+                        bar.progress.update(currData, {
+                            speed: 'DONE'
+                        });
+                        return res(bar.progress.stop(), process.exit(1));
+                    }
+                } catch (e) {
+                    console.error('\x1b[31m%s\x1b[0m', '\n Error inserting in the new collection! Probably duplicated data is already inside new DB.');
+                    return rej(process.exit(1));
+                }    
+            }));
+        });
+    } catch (e) {
+        console.error('\x1b[31m%s\x1b[0m', 'Error copying the collection!');
+        process.exit(1);
+    }
+}
+
 const main = async (sourceDbUrl, targetDbUrl, forceDrop) => {
-    return new Promise(async (res, rej) =>{})
+    return new Promise(async (res, rej) =>{
+        try {
+            const clientSource = await loadDbFromUrl(sourceDbUrl);
+            const clientTarget
+        }
+    });
 }
